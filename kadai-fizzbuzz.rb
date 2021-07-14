acbd3f9e48e0ef7608ef2c98a0b3dfeae8daff7a
noriaki.ueda <zhitianxianming@gmail.com>
2021-07-14 14:19:53 +0000

    make kadai-fizzbuzz.rb

---
+++ kadai-fizzbuzz.rb
@@ -0,0 +1,16 @@
+def fizzbuzz(number)
+  if number % 15 == 0
+    'fizzbuzz'
+  elsif number % 3 == 0
+    'fizz'
+  elsif number % 5 == 0
+    'buzz'
+  else
+    number
+  end
+end
+
+num_max = 100
+(1..num_max).each do |number|
+  puts fizzbuzz(number)
+end
